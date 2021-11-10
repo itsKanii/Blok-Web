12bea49b6d0200e2f7a88f6302716052c1c01dff
itsKanii <90227957+itsKanii@users.noreply.github.com>
2021-11-11 00:22:10 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+var hamburgerMenu = document.querySelector(".menu");
+var menuButton = document.querySelector(".icon");
+
+// menuButton.addEventListener("click", toggleHamburgerMenu);
+
+function toggleHamburgerMenu(){
+    hamburgerMenu.classList.toggle("visible");
+}
